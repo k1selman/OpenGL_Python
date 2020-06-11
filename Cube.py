ca15f12dcb2864255949342ccb75ab9226187232
Bartosz Kisiel <barkq113@gmail.com>
2020-06-11 21:07:11 +0200

    Add files via upload

--- Cube.py
+++ Cube.py
@@ -16,30 +16,60 @@ vertices = (
 )
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7),
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7),
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
+colors = (
+    (0, 0, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (1, 0, 0),
+    (0, 1, 1),
+    (1, 1, 0),
+    (1, 1, 1),
 )
 
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        #glColor3fv((15, 100, 200))
+        for vertex in surface:
+            x += 1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
+        glColor3fv((0, 0, 0))
         for vertex in edge:
             glVertex3fv(vertices[vertex])
     glEnd()
 
 def main():
     pygame.init()
-    display = (800, 600)
+    display = (1280, 720)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
@@ -53,7 +83,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        glRotatef(1, 10, 3, 5) #Now the cube is rotating
+
+        glRotatef(1, 3, 20, 5)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
